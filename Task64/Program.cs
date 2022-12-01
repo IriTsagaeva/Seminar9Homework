8fd10b09b825d0976cb6bb865203d2e94266dd22
Irina Tsagaeva <irulikk19@gmail.com>
2022-12-01 15:27:49 +0300

    Task64 added

---
+++ Task64/Program.cs
@@ -0,0 +1,29 @@
+﻿void PrintNumbers(int number)
+{
+    if (number == 1)
+    {
+        Console.Write(number + " ");
+        return;
+    }
+
+    Console.Write(number + " ");
+    PrintNumbers(number - 1);
+}
+
+Console.WriteLine("Please, enter your number");
+if (Int32.TryParse(Console.ReadLine(), out int myNumber))
+{
+    if (myNumber <= 0)
+    {
+        Console.WriteLine("Incorrect number!!! Please, enter number > 0");
+    }
+    else
+    {
+        PrintNumbers(myNumber);
+    }
+}
+else
+{
+    Console.WriteLine("Incorrect number!!!");
+}
+
