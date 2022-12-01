5f1de7cf5b169dfa41d4878f307e1f5b273bb1bb
Irina Tsagaeva <irulikk19@gmail.com>
2022-12-01 15:59:14 +0300

    Task66 added

---
+++ Task66/Program.cs
@@ -0,0 +1,40 @@
+﻿int ReturnNumbersSum(int firstNumber, int secondNumber)
+{
+    int sum = firstNumber;
+    if (firstNumber == secondNumber)
+    {
+        return sum;
+    }
+    else
+    {
+        return sum + ReturnNumbersSum(firstNumber + 1, secondNumber);
+    }
+}
+
+Console.WriteLine("Please, enter your number");
+bool myFirstNumberConverted = Int32.TryParse(Console.ReadLine(), out int myFirstNumber);
+Console.WriteLine("Please, enter your second number");
+bool mySecondNumberConverted = Int32.TryParse(Console.ReadLine(), out int mySecondNumber);
+if (mySecondNumberConverted && myFirstNumberConverted)
+{
+    if (myFirstNumber <= 0 || mySecondNumber <= 0)
+    {
+        Console.WriteLine("Incorrect numbers!!! Please, enter numbers > 0");
+    }
+    else
+    {
+        if (myFirstNumber < mySecondNumber)
+        {
+            Console.WriteLine($"Sum = {ReturnNumbersSum(myFirstNumber, mySecondNumber)}");
+        }
+        else
+        {
+            Console.WriteLine($"Sum = {ReturnNumbersSum(mySecondNumber, myFirstNumber)}");
+        }
+    }
+}
+else
+{
+    Console.WriteLine("Incorrect number!!!");
+}
+
